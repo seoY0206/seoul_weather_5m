9a9f505acbc4d2b557da30c4c3de9ec1fd11ca9f
seoY0206 <lsy19220@gmail.com>
2025-07-15 11:27:05 +0900

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,29 @@
+import requests # url:get요청
+import csv # csv로 저장
+import os # 폴더 생성
+from datetime import datetime # 시간 변환
+
+API_KEY = os.getenv("API_KEY_W")
+city = "seoul"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+response = requests.get(url)
+result = response.json()
+
+temp = result["main"]["temp"]  # 현재 기온
+humidity = result["main"]["humidity"]  # 습도
+weather = result["weather"][0]["main"]  # 날씨 상태
+current_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")  # 현재 시간
+
+# CSV header
+header = ["current_time", "weather", "temp", "humidity"]
+
+# 만약 seoul_weather.csv가 없으면 만들고 있으면 덮어쓰기
+csv_exist = os.path.exists("seoul_weather_csv")
+with open("seoul_weather.csv", "a") as file:  # a: write(있다면 덮어쓰기)
+    writer = csv.writer(file)
+          
+    if not csv_exist: # CSV가 한번도 안만들어졌다면, 헤더 추가!
+        writer.writerow(header)
+
+    writer.writerow([current_time, weather, temp, humidity])
+    print(" 서울 기온 저장 완료 ")
